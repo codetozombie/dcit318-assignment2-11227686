c063daf252e5f95aa7a03a335d66f31d03573ccf
Albert <orgin11phile@gmail.com>
2025-08-01 22:19:12 +0000

    Part3: Interfaces

--- Program.cs
+++ Program.cs
@@ -62,6 +62,27 @@ namespace OOPDemo
         {
             return Length * Width;
         }
+    }// Part 3: Interfaces
+    interface IMovable
+    {
+        void Move();
     }
 
-    
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+   
+
